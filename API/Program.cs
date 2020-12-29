f4b4d50fadb4513173b096fbcda6a119100cbeb3
YEDIDI Phaneendra <phaneendra.yedidi@hexagonsi.com>
2020-12-29 19:42:59 +0530

    Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
@@ -29,6 +25,7 @@ namespace API
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
